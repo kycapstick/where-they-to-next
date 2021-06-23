a530687fcd0d299c59abb784fc57a2f2fbed0de0
Ky Capstick <ky.capstick@gmail.com>
2021-06-23 16:27:38 -0400

    Fix errors for build

--- pages/new.tsx
+++ pages/new.tsx
@@ -1,13 +1,12 @@
 import Nav from '@/components/nav'
 import Container from '@/components/container'
-import EntryForm from '@/components/entry-form'
 
 export default function NewEntryPage() {
     return (
         <>
             <Nav title="New" />
             <Container>
-                <EntryForm />
+
             </Container>
         </>
     )
